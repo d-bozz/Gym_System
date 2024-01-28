d34c98d3395e24e7a92bf1fc5d27519384244ad9
Damian Boz <damian.boz@ukg.com>
2024-01-27 21:55:12 -0300

    validando null en actividad

--- SistemaVentaBlazor/Shared/HorarioActividadDTO.cs
+++ SistemaVentaBlazor/Shared/HorarioActividadDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,19 @@ namespace SistemaVentaBlazor.Shared
     {
         public int IdHorario { get; set; }
 
+        [Required(ErrorMessage = "El día de la semana es requerido.")]
         public string? DiaSemana { get; set; }
 
+        [Required(ErrorMessage = "La hora de inicio es requerida.")]
         public TimeSpan? HoraInicio { get; set; }
 
+        [Required(ErrorMessage = "La hora de fin es requerida.")]
         public TimeSpan? HoraFin { get; set; }
 
+        [Required(ErrorMessage = "La actividad es requerida.")]
         public string? Actividad { get; set; }
 
+        [Required(ErrorMessage = "El encargado es requerido.")]
         public string? Encargado { get; set; }
     }
 }
