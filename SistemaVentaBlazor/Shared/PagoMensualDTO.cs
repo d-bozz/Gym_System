890f5b7d7d653adb56cfb8123b2f3b2198db77c2
Damian Boz <damian.boz@ukg.com>
2024-01-31 10:58:01 -0300

    removing unused using

--- SistemaVentaBlazor/Shared/PagoMensualDTO.cs
+++ SistemaVentaBlazor/Shared/PagoMensualDTO.cs
@@ -1,27 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SistemaVentaBlazor.Shared
+﻿namespace SistemaVentaBlazor.Shared
 {
     public class PagoMensualDTO
     {
         public int IdPago { get; set; }
-
         public int? IdSocio { get; set; }
-
         public string? nombreSocio { get; set; }
-
         public string? apellidoSocio { get; set; }
-
         public int? Mes { get; set; }
-
         public int? Anio { get; set; }
-
         public DateTime? FechaPago { get; set; }
-
         public decimal? Monto { get; set; }
     }
 }
--- SistemaVentaBlazor/Shared/RolDTO.cs
+++ SistemaVentaBlazor/Shared/RolDTO.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SistemaVentaBlazor.Shared
+﻿namespace SistemaVentaBlazor.Shared
 {
     public class RolDTO
     {
         public int IdRol { get; set; }
         public string? Descripcion { get; set; }
-
         public override bool Equals(object o)
         {
             var other = o as RolDTO;
